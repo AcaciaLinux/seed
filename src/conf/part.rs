ad01b44413a52ce2e8ed3c23e857d4bea675ea60
Max Kofler <kofler.max.dev@gmail.com>
2023-02-04 21:36:28 +0100

    Added validation for filesystem existing

--- src/conf/part.rs
+++ src/conf/part.rs
@@ -1,5 +1,6 @@
 pub use super::size::*;
 use super::validate::{Validate, ValidationError};
+use crate::diskmgr::util;
 use serde::de::{self, Visitor};
 use serde::{Deserialize, Deserializer, Serialize};
 use std::fmt;
@@ -60,6 +61,48 @@ pub struct PartConf {
     pub fstab: Option<bool>,
 }
 
+impl PartConf {
+    /// Returns some context information about the partition configuration
+    fn context(&self) -> String {
+        format!("Partition {}", self.index)
+    }
+
+    /// Checks if the filesystem supplied to this config is available on the system
+    fn check_fs_available(&self) -> Result<(), ValidationError> {
+        // Retrieve the filesystem
+        let fs = match &self.fs {
+            Some(s) => s,
+            None => {
+                return Err(ValidationError::new(
+                    &self.context(),
+                    "'fs' entry is needed for checking filesystem availability",
+                ));
+            }
+        };
+
+        // Retrieve the list of available filesystems
+        let filesystems = match util::get_available_filesystems() {
+            Ok(f) => f,
+            Err(e) => {
+                return Err(ValidationError::new(
+                    &self.context(),
+                    &format!("Unable to query available filesystems: {}", e.to_string()),
+                ));
+            }
+        };
+
+        //Check if the filesystem is contained
+        if !filesystems.contains(fs) {
+            Err(ValidationError::new(
+                &self.context(),
+                &format!("Filesystem {fs} is not available on the system"),
+            ))
+        } else {
+            Ok(())
+        }
+    }
+}
+
 impl Validate for PartConf {
     fn validate(&mut self) -> Result<(), ValidationError> {
         //Match the actions with their allowed entries
@@ -92,6 +135,8 @@ impl Validate for PartConf {
                         self.index.to_string().as_str(),
                         "'fs' is required when partition action is set to 'format'",
                     ));
+                } else {
+                    self.check_fs_available()?;
                 }
             }
             //When resizing, a size is required, but fs can't be changed
@@ -117,11 +162,15 @@ impl Validate for PartConf {
                         "'size' is required when action is 'create'",
                     ));
                 }
-                if self.mount.is_some() && self.fs.is_none() {
-                    return Err(ValidationError::new(
-                        self.index.to_string().as_str(),
-                        "'fs' is required when new partition is mounted",
-                    ));
+                if self.mount.is_some() {
+                    if self.fs.is_none() {
+                        return Err(ValidationError::new(
+                            self.index.to_string().as_str(),
+                            "'fs' is required when new partition is mounted",
+                        ));
+                    } else {
+                        self.check_fs_available()?;
+                    }
                 }
             }
         }
